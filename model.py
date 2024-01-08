34adaaf5d1aa20adaa78a7f804e83cb2b5e94b14
Meryl Lou Cabio <meryllou7@gmail.com>
2024-01-08 15:15:18 -0600

    Added games

--- model.py
+++ model.py
@@ -12,16 +12,26 @@ class Game(db.Model):
     description = db.Column(db.String(100))
 
 
-def connect_to_db(app, db_uri="postgresql:///games"):
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-    db.app = app
-    db.init_app(app)
-
-
 def example_data():
     """Create example data for the test database."""
     # FIXME: write a function that creates a game and adds it to the database.
-    print("FIXME")
+    # print("FIXME")
+    Game.query.delete()
+
+    gttr = Game(game_id=1, name='Ticket to Ride', description='a cross-country train adventure')
+    gpg = Game(game_id=2, name='Power Grid', description='supply the most cities with power')
+    gal = Game(game_id=3, name='Amazing Labyrinth', description='move around the shifting paths of the labyrinth in a race to collect various treasures')
+    gpof = Game(game_id=4, name='Princes of Florence', description='attract artists and scholars trying to become the most prestigious family in Florence')
+    ga = Game (game_id=5, name='Agricola', description='farmers sow, plow the fields, collect wood, and feed their families')
+    
+    db.session.add_all([gttr, gpg, gal, gpof, ga])
+    db.session.commit()
+
+def connect_to_db(app, db_uri="postgresql:///games"):
+# def connect_to_db(app, db_uri="postgresql:///testdb"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    db.app = app
+    db.init_app(app)
 
 
 if __name__ == '__main__':
